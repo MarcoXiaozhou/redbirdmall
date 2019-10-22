65823541a22a0b1fc1092102de36268038ccc8b7
MARCO <fanwenzhou0603@163.com>
2019-10-22 10:47:07 +0800

    redbird项目用户信息服务

---
+++ redbird-user/src/main/java/com/redbird/marco/redbirduser/RedbirdUserApplication.java
@@ -0,0 +1,13 @@
+package com.redbird.marco.redbirduser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedbirdUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedbirdUserApplication.class, args);
+    }
+
+}
---
+++ redbird-user/src/test/java/com/redbird/marco/redbirduser/RedbirdUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redbird.marco.redbirduser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedbirdUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
